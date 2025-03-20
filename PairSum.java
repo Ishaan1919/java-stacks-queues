5495af31a3791a6a84d1cbfb85495bad4a3a5a08
Ishaan <ishaan0642.be21@chitkara.edu.in>
2025-03-20 14:39:27 +0530

    Adding function to the pair sum program to get indexs of the pair sum elements

--- PairSum.java
+++ PairSum.java
@@ -7,11 +7,29 @@ import java.util.HashMap;
 
 public class PairSum {
     public static void main(String[] args) {
-        int[] nums = {2,7,11,15,3,5,4,5,7,3};
+        int[] nums = {2,7,11,15,3,5,4,5,4,7,3};
         int target = 9;
 
-        ArrayList<Pair> pairSum = getPairSumElements(nums,target);
+//        ArrayList<Pair> pairSum = getPairSumElements(nums,target);
+        ArrayList<Pair> pairSum = getPairSumIndexes(nums,target);
         ZeroSumSubarrays.display(pairSum);
+    }
+
+    public static ArrayList<Pair> getPairSumIndexes(int[] arr, int target){
+        ArrayList<Pair> ans = new ArrayList<>();
+
+        int sum = 0;
+        int n = arr.length;
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int i=0;i<n;i++){
+            if(map.containsKey(target-arr[i])){
+                    ans.add(new Pair(i,map.get(target-arr[i])));
+            }
+            map.put(arr[i],i);
+        }
+
+        return ans;
     }
 
     public static ArrayList<Pair> getPairSumElements(int[] arr, int target){
